646099d72437109baf1e7321b206fd7d7802dff1
Ray <r.samwise@gmail.com>
2019-08-26 17:39:03 +0400

    2

--- src/Loader.java
+++ src/Loader.java
@@ -18,6 +18,16 @@ public class Loader
         two.drink(two.getWeight());
         System.out.println("Кошка напилась: " + two.getStatus());
 
+        System.out.println("Вес котиков");
+        System.out.println("Вес тома: " + tom.getWeight());
+        System.out.println("Вес бара: " + bar.getWeight());
+        System.out.println("Вес первого: " + one.getWeight());
+        System.out.println("Вес второго: " + two.getWeight());
+        System.out.println("Вес третьего: " + three.getWeight());
+        System.out.println("Вес четвертого: " + four.getWeight());
+        System.out.println("Вес пятого: " + five.getWeight());
+
+
         System.out.println("Взрываем Тома");
         while (tom.getWeight() < tom.maxWeight)
         {
@@ -26,11 +36,13 @@ public class Loader
             System.out.println(tom.getStatus());
         }
 
-       // while (bar.getStatus() != "Dead")
-    //    {
-    //        bar.meow();
-   //         System.out.println(bar.getStatus());
-   //     }
+        while (bar.getStatus() != "Dead")
+        {
+            bar.meow();
+        }
+        System.out.println(bar.getStatus());
+
+
 
     }
 }
